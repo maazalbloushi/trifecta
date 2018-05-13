e66e323870a0499fd7c538117eaf0ec430c19b71
Azwa Kamaruddin <scholar@azwa.me>
2018-05-14 03:51:38 +0800

    Final commit
    Map analysis - made tables better
    Documentation - added screenshots
    About - fixed grammar

--- shiny/app.R
+++ shiny/app.R
@@ -25,7 +25,7 @@ ui <- fluidPage(
                                                    fluidRow(
                                                         
                                                        # Map of Malaysia:
-                                                       box(width = 12, title = HTML("<font size='4'>Map of Malaysia</font>"), 
+                                                       box(width = 12, title = HTML("<font size='3'><u>Map of Malaysia</u></font>"), 
                                                            leafletOutput("tableMalaysiaMap", height = 400),
                                                            br()
                                                            ),
@@ -59,8 +59,13 @@ ui <- fluidPage(
 
                                                    
                                                    # State Demography
-                                                   fluidRow(
-                                                       box(width = 8, title = HTML("<font size='4'>&nbsp;&nbsp;&nbsp;State Demography</font>"), tableOutput('table'))
+                                                   column(width = 12,
+                                                       fluidRow(
+                                                           box(width = 12, title = HTML("<font size='3'><u>State Demography</u></font>"), tableOutput('table'))
+                                                       ),
+                                                       fluidRow(
+                                                           HTML("&nbsp;&nbsp;&nbsp; * Other than Elementary Occupation (Pekerjaan Asas)")
+                                                       )
                                                    )
                                                 )
                                             ),
@@ -68,7 +73,7 @@ ui <- fluidPage(
                                             # Right Side - Student Enrollment:
                                             column(width = 5, 
                                                    fluidRow(
-                                                       box(width = 12, title = HTML("<font size='4'>&nbsp;&nbsp;&nbsp;Student Enrollment and Career Opportunity</font>"), tableOutput('tableEnrollmentCareer'))
+                                                       box(width = 12, title = htmlOutput('selectedState'), tableOutput('tableEnrollmentCareer'))
                                                    )
                                             )
                                     )
@@ -79,19 +84,20 @@ ui <- fluidPage(
                                         
                                         column(width = 12, 
                                                p(
-                                                   HTML("This Shiny application presents several information about the availability of talents and career prospects for several states in Malaysia.")
+                                                   HTML("This Shiny application presents several information about the availability of talent and career opportunity for several states in Malaysia. <br> We have selected 7 different states that we see strategic to make Malaysia a developed country with the right competencies by 2020.")
                                                ),
                                                br(),
                                                p(
                                                    HTML("<h4>The Map Widget</h4>"),
+                                                   imageOutput("img_map", width = 584, height = 260),
                                                    p(
-                                                       HTML("The main widget is the Map, which shows the number of students currently enrolled in vocational college for a particular state and is designated by a blue circle. Within the same map, the number of available jobs for each selected state are also presented by a red circle. The size of the circle indicates how large or small the availability of either talent or career opportunity. If the talent circle (blue) is smaller than the job opportunity circle (red), it means that there is a shortage of skilled workers to fill in those job demands. If the situation is reversed, it shows that there is an excess of talents but no jobs to fulfil them. This relationship shows how skilled workers in Malaysia could make the decision to move to a different state in order to find jobs available for their sector.")
+                                                       HTML("The main widget is the Map, which shows the number of students currently enrolled in vocational colleges for a particular state and is designated by a blue circle. These students are referred to as talents. Within the same map, the number of available jobs for each selected state are also presented by a red circle. The size of the circle indicates how large or small the availability of either talent or career opportunity. If the talent circle (blue) is smaller than the job opportunity circle (red), it means that there is a shortage of skilled workers to fill in those job demands. If the situation is reversed, it shows that there is an excess of talents but no jobs to fulfil them. This relationship shows how skilled workers in Malaysia could make the decision to move to a different state in order to find jobs available for their sector.")
                                                    )
-                                                        
                                                ),
                                                br(),
                                                p(
                                                    HTML("<h4>The Legend Checkbox Widget</h4>"),
+                                                   imageOutput("img_legend", width = 137, height = 82),
                                                    p(
                                                        HTML("Two main types of data are presented within the map and can be controlled by the Legend checkboxes. To show only the available talents designated by blue circles inside the map, you may check the “Talent availability” checkbox and uncheck the “Job opportunity” checkbox. To show only the job opportunity designated by red circles inside the map, check the “Job opportunity” checkbox and uncheck the “Talent availability” checkbox. Checking or unchecking both checkboxes will show or hide both types of data on the map.")
                                                    )
@@ -99,6 +105,7 @@ ui <- fluidPage(
                                                br(),
                                                p(
                                                    HTML("<h4>The States Dropdown Widget</h4>"),
+                                                   imageOutput("img_states", width = 133, height = 270),
                                                    p(
                                                        HTML("You can use the State dropdown widget to see the information for other states in Malaysia. By selecting a state using this widget, the map will move to that particular state and display the circles representing both talent availability and job opportunity according to their designated colours.")
                                                    )
@@ -106,8 +113,23 @@ ui <- fluidPage(
                                                br(),
                                                p(
                                                    HTML("<h4>The Map Zoom Widget</h4>"),
+                                                   imageOutput("img_zoom", width = 291, height = 101),
                                                    p(
-                                                       HTML("In order to zoom in or out of the map, you may drag the slider on the Map Zoom widget. The maximum zoom out value is 1 and zoom in value is 20.")
+                                                       HTML("In order to zoom in or out of the map, you may drag the slider on the Map Zoom widget. The minimum zoom out value is 1 and the maximum zoom in value is 20.")
+                                                   )
+                                               ),
+                                               br(),
+                                               p(
+                                                   HTML("<h4>State Demography Table</h4>"),
+                                                   p(
+                                                       HTML("The State Demography table gives a summary of the total number of talents and career opportunities for each state. It also shows the sector with the highest number of employment other than elementary occupation.")
+                                                   )
+                                               ),
+                                               br(),
+                                               p(
+                                                   HTML("<h4>Student Enrollment and Career Opportunity Table</h4>"),
+                                                   p(
+                                                       HTML("The Student Enrollment and Career Opportunity table lists out the details of the courses that vocational college students are enrolled in for the selected state as well as the number of career opportunities related to those courses in the same state.<br><br>")
                                                    )
                                                )
                                         )
@@ -119,7 +141,27 @@ ui <- fluidPage(
 
                                         column(width = 12,
                                             p(
-                                                HTML("This Shiny application is developed to show the relationship between the availability of talents and career opportunities in Malaysia by looking at the datasets from The Malaysian Administrative Modernisation and Management Planning Unit (MAMPU). The link between talents and job prospects are made by combining both the data of Vacancies for Non-graduates by State and Occupational Category (Sector) with the data of Vocational College Enrollments by State.")
+                                                HTML("This Shiny application is developed to show the relationship between the availability of talents and career opportunities in several states of Malaysia by looking at the datasets from The Malaysian Administrative Modernisation and Management Planning Unit (MAMPU). The link between talents and job prospects are made by combining both the data of Vacancies for Non-graduates by State and Occupational Category with the data of Vocational College Enrollments by State."),
+                                                HTML("<br><br>The application is developed using R language on RStudio and deployed on the Shinyapps.io server. Various widgets are used to present the data that we have accumulated and processed. The application is useful for job seekers to find out which job sector has the highest or lowest number of job offerings per state. It is also useful for Employers to find out which state has the most talent for their sector.")
+                                            ),
+                                            br(),
+                                            p(
+                                                HTML("<h4>The Course</h4>"),
+                                                p(
+                                                    HTML("This project is part of the group assignment for the Principles of Data Science course (WQD7001)"),
+                                                    HTML("<br>Master of Data Science - Faculty of Computer Science and Information Technology"),
+                                                    HTML("<br>University of Malaya"),
+                                                    HTML("<br>Date of completion: 15th of May 2018")
+                                                )
+                                            ),
+                                            br(),
+                                            p(
+                                                HTML("<h4>The Team</h4>"),
+                                                p(
+                                                    HTML("Kok Hon Loong - WQD170086"),
+                                                    HTML("<br>Ahmad Azwa Kamaruddin - WQD170089"),
+                                                    HTML("<br>Mohamed Firdaus Shajahan - WQD170070")
+                                                )
                                             )
                                         )
                                     )
@@ -131,10 +173,10 @@ ui <- fluidPage(
 # SERVER SIDE
 # --------------- #
 server <- function(input, output) {
-  
+    
     # Display the State Demography
-    datdisplay <- read.csv(file = "dataset/datasetdisplay.csv")
-    output$table <- renderTable(datdisplay)
+    datdisplay <- read.csv(file = "dataset/datasetdisplay.csv", header = TRUE, check.names = FALSE)
+    output$table <- renderTable(datdisplay, hover = TRUE, striped = TRUE, bordered = TRUE, width = 600)
   
     # Read the full state demography data (with longitude and latitude)
     dat <- read.csv(file = "dataset/dataset.csv")
@@ -143,43 +185,107 @@ server <- function(input, output) {
     values <- reactiveValues(dat2 = NULL) #for reset dataframe value when new state selected.
     observeEvent(input$states, {
         csvSelected <- paste("stateData/",input$states,".csv",sep = "") #filepath creation with selectedInput
-        values$dat2 <- read.csv(csvSelected) #load the csv
+        values$dat2 <- read.csv(csvSelected, header = TRUE, check.names = TRUE) #load the csv
     })
   
-    output$tableEnrollmentCareer <- renderTable(values$dat2) #render the table
+    output$tableEnrollmentCareer <- renderTable(values$dat2, hover = TRUE, striped = TRUE, bordered = TRUE, na = "n/a", width = "auto") #render the table
+    
+    # Render reactive text based on seleted state
+    output$selectedState <- renderText({
+            if (is.element('1', input$states)) {
+                namedState <- "<font size='3'><u>K. Lumpur</u></font>"
+            } else if (is.element('2', input$states)) {
+                namedState <- "<font size='3'><u>Selangor</u></font>"
+            } else if (is.element('3', input$states)) {
+                namedState <- "<font size='3'><u>P. Pinang</u></font>"
+            } else if (is.element('4', input$states)) {
+                namedState <- "<font size='3'><u>Johor</u></font>"
+            } else if (is.element('5', input$states)) {
+                namedState <- "<font size='3'><u>Perak</u></font>"
+            } else if (is.element('6', input$states)) {
+                namedState <- "<font size='3'><u>Kelantan</u></font>"
+            } else if (is.element('7', input$states)) {
+                namedState <- "<font size='3'><u>Sarawak</u></font>"
+            } else {
+                namedState <- "<font size='3'><u>undefined</u></font>"
+            }
+        
+            paste("<font size='3'><u>Student Enrollment and Career Opportunity for </u></font>", namedState)
+        })
     
     # Malaysian Map
     output$tableMalaysiaMap <- renderLeaflet({
-    zoomV <- input$obs
+        zoomV <- input$obs
+        
+        dat_Long = dat[input$states,"Longitude"]
+        dat_Lat = dat[input$states,"Latitude"]
+        
+        if (is.element('jobs', input$supplydemand)) {
+          demandC <- 'red'
+        } else {
+          demandC <- 'none'
+        }
+        
+        if (is.element('talents', input$supplydemand)) {
+          supplyC <- 'blue'
+        } else {
+          supplyC <- 'none'
+        }
+        
+        geoData <- readLines("json/malaysia.geojson") %>% paste(collapse = "\n")
+        
+        leaflet(dat) %>% addTiles() %>%
+          setView(lng = dat_Long, lat = dat_Lat, zoom = zoomV) %>%
+          addProviderTiles(providers$CartoDB.Positron, options = providerTileOptions(noWrap = TRUE)) %>%
+          addGeoJSON(geoData, weight = 2, color = "#000", fill = FALSE) %>%
+          addCircles(lng = ~Longitude, lat = ~Latitude, weight = 2,
+                     radius = ~sqrt(Demand) * 50, popup = ~States, color = demandC
+          ) %>%
+          addCircles(lng = ~Longitude, lat = ~Latitude, weight = 2,
+                     radius = ~sqrt(Supply) * 50, popup = ~States, color = supplyC
+          )
+      })
     
-    dat_Long = dat[input$states,"Longitude"]
-    dat_Lat = dat[input$states,"Latitude"]
+    # Images:
+    output$img_map <- renderImage({
+        outfile <- "images/img_map.jpeg"
+        
+        # Return a list
+        list(src = outfile,
+             width = 584,
+             height = 250
+        )
+    }, deleteFile = FALSE)
     
-    if (is.element('jobs', input$supplydemand)) {
-      demandC <- 'red'
-    } else {
-      demandC <- 'none'
-    }
+    output$img_legend <- renderImage({
+        outfile <- "images/img_legend.jpeg"
+        
+        # Return a list
+        list(src = outfile,
+             width = 137,
+             height = 82
+        )
+    }, deleteFile = FALSE)
     
-    if (is.element('talents', input$supplydemand)) {
-      supplyC <- 'blue'
-    } else {
-      supplyC <- 'none'
-    }
+    output$img_states <- renderImage({
+        outfile <- "images/img_states.jpeg"
+        
+        # Return a list
+        list(src = outfile,
+             width = 133,
+             height = 260
+        )
+    }, deleteFile = FALSE)
     
-    geoData <- readLines("json/malaysia.geojson") %>% paste(collapse = "\n")
-    
-    leaflet(dat) %>% addTiles() %>%
-      setView(lng = dat_Long, lat = dat_Lat, zoom = zoomV) %>%
-      addProviderTiles(providers$CartoDB.Positron, options = providerTileOptions(noWrap = TRUE)) %>%
-      addGeoJSON(geoData, weight = 2, color = "#000", fill = FALSE) %>%
-      addCircles(lng = ~Longitude, lat = ~Latitude, weight = 2,
-                 radius = ~sqrt(Demand) * 50, popup = ~States, color = demandC
-      ) %>%
-      addCircles(lng = ~Longitude, lat = ~Latitude, weight = 2,
-                 radius = ~sqrt(Supply) * 50, popup = ~States, color = supplyC
-      )
-  })
+    output$img_zoom <- renderImage({
+        outfile <- "images/img_zoom.jpeg"
+        
+        # Return a list
+        list(src = outfile,
+             width = 291,
+             height = 101
+        )
+    }, deleteFile = FALSE)
 }
 
 # --------------- #
